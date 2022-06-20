3ca9698dcda69a2372fe9ebb99e707676c42aa5c
Aloiis <86479028+Aloiis@users.noreply.github.com>
2022-06-20 16:25:28 -0300

    Finalizado

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,18 +1,37 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { CloseButton } from "../../CloseButton"
 import { FeedbackType, feedbackTypes } from "../Index"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
+    onFeedbackSent: () => void;
 }
 
 
 
-export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
+
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
+    const [comment, setComment] = useState('');
 
     const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault()
+
+        console.log({
+            screenshot,
+            comment,
+        })
+        
+        onFeedbackSent();
+    }
 
     return (
         <>
@@ -30,9 +49,23 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
+            <form className="my-4 w-full" onSubmit={handleSubmitFeedback} >
+                <textarea 
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:outline-none focus:ring-1 resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
+                />
+                <footer className="flex gap-2 mp-2">
+                    <ScreenshotButton  
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
 
-            </div>
+                    <button type="submit" disabled={comment.length == 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
+                        Enviar feedback
+                    </button>
+                </footer>
+            </form>
         </>
     )
 }
